90d36812fb0a008ef0b0db7b36d93c888178bb6f
Dongbin Yoo <dongbin420@gmail.com>
2025-06-12 19:15:03 +0900

    Time: 22 ms (39.97%), Space: 73.8 MB (58.51%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -2,24 +2,25 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
-    const freqOfNums = new Map();
-
-    for (const num of nums) {
-        freqOfNums.set(num, (freqOfNums.get(num) || 0) + 1);
-    }
-
-    for (const value of freqOfNums.values()) {
-        if (value >= 2) return true;
-    }
-
-    return false;
-};
-
 // var containsDuplicate = function(nums) {
-//     const setOfNums = new Set(nums);
+//     const freqOfNums = new Map();
 
-//     if (setOfNums.size === nums.length) return false;
+//     for (const num of nums) {
+//         freqOfNums.set(num, (freqOfNums.get(num) || 0) + 1);
+//     }
 
-//     return true;
+//     for (const value of freqOfNums.values()) {
+//         if (value >= 2) return true;
+//     }
+
+//     return false;
 // };
+
+// 이 문제에 대한 '해결법'만을 고려한다면, 이 풀이가 더 좋은 풀이. 
+var containsDuplicate = function(nums) {
+    const setOfNums = new Set(nums);
+
+    if (setOfNums.size === nums.length) return false;
+
+    return true;
+};
