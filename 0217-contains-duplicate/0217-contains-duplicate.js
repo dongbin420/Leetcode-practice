454a4d4f0a63a24915560188ed860003ef762b0e
Dongbin Yoo <dongbin420@gmail.com>
2025-06-12 19:09:58 +0900

    Time: 22 ms (39.97%), Space: 73.9 MB (54.62%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -2,24 +2,24 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
-    const freqOfNums = new Map();
-
-    for (const num of nums) {
-        freqOfNums.set(num, (freqOfNums.get(num) || 0) + 1);
-    }
-
-    for (const value of freqOfNums.values()) {
-        if (value >= 2) return true;
-    }
-
-    return false;
-};
-
 // var containsDuplicate = function(nums) {
-//     const setOfNums = new Set(nums);
+//     const freqOfNums = new Map();
 
-//     if (setOfNums.size === nums.length) return false;
+//     for (const num of nums) {
+//         freqOfNums.set(num, (freqOfNums.get(num) || 0) + 1);
+//     }
 
-//     return true;
+//     for (const value of freqOfNums.values()) {
+//         if (value >= 2) return true;
+//     }
+
+//     return false;
 // };
+
+var containsDuplicate = function(nums) {
+    const setOfNums = new Set(nums);
+
+    if (setOfNums.size === nums.length) return false;
+
+    return true;
+};
