767f3846bea1d2d430d3c7439222b69e886c9501
Dongbin Yoo <dongbin420@gmail.com>
2025-06-12 19:08:00 +0900

    Time: 33 ms (26.9%), Space: 78.5 MB (8%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const freqOfNums = new Map();
+
+    for (const num of nums) {
+        freqOfNums.set(num, (freqOfNums.get(num) || 0) + 1);
+    }
+
+    for (const value of freqOfNums.values()) {
+        if (value >= 2) return true;
+    }
+
+    return false;
+};
+
+// var containsDuplicate = function(nums) {
+//     const setOfNums = new Set(nums);
+
+//     if (setOfNums.size === nums.length) return false;
+
+//     return true;
+// };
