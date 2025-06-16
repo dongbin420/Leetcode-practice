75d96b2618206e78ef302846b77209f69a667928
Dongbin Yoo <dongbin420@gmail.com>
2025-06-17 01:05:09 +0900

    Time: 11 ms (37.87%), Space: 69.4 MB (77%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,26 +3,46 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    const answer = [];
-    const allProduct = nums.reduce((acc, cur) => acc * cur, 1);
+    const answer = Array(nums.length).fill(1);
+    let prefixProd = 1;
+    let suffixProd = 1;
 
-    if (allProduct === 0) {
-        for (let i = 0; i < nums.length; i++) {
-            if (nums[i] === 0) {
-                answer[i] = nums.reduce((acc, cur, idx) => {
-                    if (idx === i) return acc;
-                    
-                    return acc * cur;
-                }, 1);
-            } else {
-                answer[i] = allProduct;
-            }
-        }
-    } else {
-        for (let j = 0; j < nums.length; j++) {
-            answer[j] = allProduct / nums[j];
-        }
+    for (let i = 0; i < nums.length; i++) {
+        answer[i] *= prefixProd;
+        prefixProd *= nums[i];
+    }
+
+    for (let j = nums.length - 1; j >= 0; j--) {
+        answer[j] *= suffixProd;
+        suffixProd *= nums[j];
     }
 
     return answer;
-};
+}
+
+
+// 처음 푼 방식(나눗셈 활용)
+// var productExceptSelf = function(nums) {
+//     const answer = [];
+//     const allProduct = nums.reduce((acc, cur) => acc * cur, 1);
+
+//     if (allProduct === 0) {
+//         for (let i = 0; i < nums.length; i++) {
+//             if (nums[i] === 0) {
+//                 answer[i] = nums.reduce((acc, cur, idx) => {
+//                     if (idx === i) return acc;
+                    
+//                     return acc * cur;
+//                 }, 1);
+//             } else {
+//                 answer[i] = allProduct;
+//             }
+//         }
+//     } else {
+//         for (let j = 0; j < nums.length; j++) {
+//             answer[j] = allProduct / nums[j];
+//         }
+//     }
+
+//     return answer;
+// };
