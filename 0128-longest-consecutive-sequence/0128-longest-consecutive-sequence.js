29d467cc960d5b7aa37212e8306167caa3a5c0f1
Dongbin Yoo <dongbin420@gmail.com>
2025-06-18 22:50:39 +0900

    Time: 32 ms (94.66%), Space: 79.4 MB (82.77%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -3,22 +3,48 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    if (nums.length === 0) {
-        return 0;
-    }
-
+    if (nums.length === 0) return 0;
+    
+    const numSet = new Set(nums);
     const cnts = [];
-    let cnt = 1;
-    const sortedNums = Array.from(new Set(nums)).sort((a, b) => a - b); 
 
-    for (let i = 0; i < nums.length; i++) {
-        if (i + 1 < nums.length && sortedNums[i + 1] === sortedNums[i] + 1) {
-            cnt++
-        } else {
+    for (const num of numSet.values()) {
+        if (!numSet.has(num - 1)) {
+            let cnt = 1;
+            let i = 1;
+
+            while (numSet.has(num + i)) {
+                cnt++;
+                i++;
+            }
+
             cnts.push(cnt);
-            cnt = 1;
+        } else {
+            continue;
         }
     }
 
     return Math.max(...cnts);
-};
+}
+
+// nlog n 해결 방식(sort 메서드가 nlog n이므로)
+// var longestConsecutive = function(nums) {
+//     if (nums.length === 0) {
+//         return 0;
+//     }
+
+//     const cnts = [];
+//     let cnt = 1;
+//     const sortedNums = Array.from(new Set(nums)).sort((a, b) => a - b); 
+
+//     for (let i = 0; i < nums.length; i++) {
+//         if (i + 1 < nums.length && sortedNums[i + 1] === sortedNums[i] + 1) {
+//             cnt++
+//         } else {
+//             cnts.push(cnt);
+//             cnt = 1;
+//         }
+//     }
+
+//     return Math.max(...cnts);
+// };
