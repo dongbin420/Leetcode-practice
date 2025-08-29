0b93869b22823ca7f4f9cc9acc2053ead3049f8a
Dongbin Yoo <dongbin420@gmail.com>
2025-08-29 17:31:29 +0900

    Time: 3 ms (49.29%), Space: 57.4 MB (15.83%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -2,26 +2,53 @@
  * @param {string} s
  * @return {boolean}
  */
+// var isValid = function(s) {
+//     const stack = [];
+
+//     for (let i = 0; i < s.length; i++) {
+//         if (s[i] === '(' || s[i] === '[' || s[i] === '{') {
+//             stack.push(s[i]);
+//         } else {
+//             if (stack.length === 0) return false; // 여는 괄호 없이 닫는 괄호가 나오면 조기 실패
+            
+//             if (s[i] === ')') {
+//                 const last = stack.pop();
+
+//                 if (last !== '(') return false;
+//             } else if (s[i] === ']') {
+//                 const last = stack.pop();
+
+//                 if (last !== '[') return false;
+//             } else if (s[i] === '}') {
+//                 const last = stack.pop();
+
+//                 if (last !== '{') return false;
+//             }
+
+//         }
+//     }
+
+//     return stack.length === 0;
+// };
+
 var isValid = function(s) {
     const stack = [];
 
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === '(' || s[i] === '[' || s[i] === '{') {
-            stack.push(s[i]);
-        } else if (s[i] === ')') {
-            const last = stack.pop();
+  for (p of s) {
+    if (p === '(' || p === '{' || p === '[') {
+      stack.push(p);
+    } else {
+      const last = stack.pop();
 
-            if (last !== '(') return false;
-        } else if (s[i] === ']') {
-            const last = stack.pop();
-
-            if (last !== '[') return false;
-        } else if (s[i] === '}') {
-            const last = stack.pop();
-
-            if (last !== '{') return false;
-        }
+      if (p === ')' && last !== '(') {
+        return false;
+      } else if (p === '}' && last !== '{') {
+        return false;
+      } else if (p === ']' && last !== '[') {
+        return false;
+      }
     }
+  }
 
-    return stack.length === 0;
+  return stack.length === 0;
 };
