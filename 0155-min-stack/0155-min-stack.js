a35d6ac09ca191c23eec50bf9c0ce63e54340bea
Dongbin Yoo <dongbin420@gmail.com>
2025-08-29 19:43:09 +0900

    Time: 135 ms (8.37%), Space: 65.2 MB (73.36%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.js
@@ -0,0 +1,45 @@
+
+var MinStack = function() {
+    this.stack = [];
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    this.stack.push(val);
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    this.stack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    // Math.min을 사용하지 않고, 직접 배열의 요소들을 비교해서 O(1)의 시간 복잡도로 최솟값을 구해야 함.
+    
+
+    return Math.min(...this.stack);
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
