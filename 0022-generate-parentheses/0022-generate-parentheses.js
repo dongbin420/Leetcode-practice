82ddecd12c56aa6313fd7527936b491f1021cc1c
Dongbin Yoo <dongbin420@gmail.com>
2025-09-04 18:52:10 +0900

    Time: 2 ms (44.99%), Space: 55.6 MB (20%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.js
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -2,31 +2,61 @@
  * @param {number} n
  * @return {string[]}
  */
-var generateParenthesis = function(n) {
-    const answer = [];
 
-    const makeParentheses = (open, close, str) => {
-        if (open < 0 || close < 0) return;
-        if (close < open) return;
+// 세번째 시도(스택 이용)
+ var generateParenthesis = function(n) {
+    const answer = [];
+    const stack = [{s: '', open: n, close: n}];
+
+    while (stack.length > 0) {
+        const { s, open, close } = stack.pop();
 
         if (open === 0 && close === 0) {
-            answer.push(str);
-            return;
+            answer.push(s);
+            continue;
         }
 
         if (open > 0) {
-            makeParentheses(open - 1, close, str + '(');
+            stack.push({s: s + '(', open: open - 1, close});
         }
-        
-        if (close > open) {
-            makeParentheses(open, close - 1, str + ')');
-        }
-    }
 
-    makeParentheses(n, n, '');
+        if (close > open) {
+            stack.push({s: s + ')', open, close: close - 1});
+        }
+    } 
 
     return answer;
 };
+
+
+
+
+// 두번째 시도(맞은 풀이)
+// var generateParenthesis = function(n) {
+//     const answer = [];
+
+//     const makeParentheses = (open, close, str) => {
+//         if (open < 0 || close < 0) return;
+//         if (close < open) return;
+
+//         if (open === 0 && close === 0) {
+//             answer.push(str);
+//             return;
+//         }
+
+//         if (open > 0) {
+//             makeParentheses(open - 1, close, str + '(');
+//         }
+        
+//         if (close > open) {
+//             makeParentheses(open, close - 1, str + ')');
+//         }
+//     }
+
+//     makeParentheses(n, n, '');
+
+//     return answer;
+// };
 
 
 
