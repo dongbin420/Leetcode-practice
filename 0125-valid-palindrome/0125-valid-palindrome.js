0e09e0255bb17c97651cf560230310af4b9df383
Dongbin Yoo <dongbin420@gmail.com>
2025-09-20 00:39:25 +0900

    Time: 3 ms (86.77%), Space: 58.5 MB (46.51%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,29 +3,29 @@
  * @return {boolean}
  */
 // 개선한 투포인터 방식
-// var isPalindrome = function(s) {
-//     // 문자의 왼쪽 인덱스와 오른쪽 인덱스를 가리키는 left와 right
-//     let left = 0, right = s.length - 1;
+var isPalindrome = function(s) {
+    // 문자의 왼쪽 인덱스와 오른쪽 인덱스를 가리키는 left와 right
+    let left = 0, right = s.length - 1;
 
-//     // 주어진 입력 문자가 Alphanumeric인지 확인하는 함수
-//     const isAlnum = (ch) => {
-//         return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9');
-//     }
+    // 주어진 입력 문자가 Alphanumeric인지 확인하는 함수
+    const isAlnum = (ch) => {
+        return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9');
+    }
 
-//     // 문자가 짝수인 경우는, right가 left보다 커지는 시점부터 검사할 필요가 없어지고, 문자가 홀수인 경우는 right와 left가 같아지는 시점부터 검사할 필요가 없어진다. 따라서 left가 right가 작을때만 검사하면 되는 것.
-//     while (left < right) {
-//         // left < right를 여기에서 한번 더 하는 이유는, left와 right가 조정되는 코드가 있어서, 이 시점에서도 left가 right를 넘게 되는 경우가 있기 때문
-//         while (left < right && !isAlnum(s[left])) left++;
-//         while (left < right && !isAlnum(s[right])) right--;
+    // 문자가 짝수인 경우는, right가 left보다 커지는 시점부터 검사할 필요가 없어지고, 문자가 홀수인 경우는 right와 left가 같아지는 시점부터 검사할 필요가 없어진다. 따라서 left가 right가 작을때만 검사하면 되는 것.
+    while (left < right) {
+        // left < right를 여기에서 한번 더 하는 이유는, left와 right가 조정되는 코드가 있어서, 이 시점에서도 left가 right를 넘게 되는 경우가 있기 때문
+        while (left < right && !isAlnum(s[left])) left++;
+        while (left < right && !isAlnum(s[right])) right--;
 
-//         if (s[left].toLowerCase() !== s[right].toLowerCase()) return false;
+        if (s[left].toLowerCase() !== s[right].toLowerCase()) return false;
 
-//         left++;
-//         right--;
-//     }
+        left++;
+        right--;
+    }
 
-//     return true;
-// }
+    return true;
+}
 
 
 // 내가 스스로 푼 투포인터 방식
@@ -69,31 +69,3 @@
 
 //     return false;
 // };
-
-
-const isPalindrome = (s) => {
-  let testingStr = '';
-  const isAlnum = (ch) => {
-        return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9');
-    }
-
-  for (const c of s.toLowerCase()) {
-    if (isAlnum(c)) {
-      testingStr += c;
-    }
-  }
-
-  let front = 0;
-  let back = testingStr.length - 1;
-
-  while (front < back) {
-    if (testingStr[front] !== testingStr[back]) {
-      return false;
-    }
-
-    front++;
-    back--;
-  }
-
-  return true;
-}
