a7d870bdfd9a9e9d5188879580b8aa738fe04549
Dongbin Yoo <dongbin420@gmail.com>
2025-10-02 19:51:18 +0900

    Time: 16 ms (14.03%), Space: 60.8 MB (15.09%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.js
+++ 0994-rotting-oranges/0994-rotting-oranges.js
@@ -2,51 +2,86 @@
  * @param {number[][]} grid
  * @return {number}
  */
+// var orangesRotting = function(grid) {
+//     const row = grid.length;
+//     const col = grid[0].length;
+//     const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+//     const queue = []
+//     let answer = 0;
+//     let fresh = 0;
+
+//     for (let i = 0; i < row; i++) {
+//         for (let j = 0; j < col; j++) {
+//             if (grid[i][j] === 2) {
+//                 queue.push([i, j, 0]);
+//             } else if (grid[i][j] === 1) {
+//                 fresh++;
+//             }
+//         }
+//     }
+
+//     while (queue.length > 0) {
+//             const [x, y, minute] = queue.shift();
+
+//             for (const [dx, dy] of directions) {
+//                 const nx = x + dx;
+//                 const ny = y + dy;
+
+//                 if (nx >= 0 && ny >= 0 && nx < row && ny < col && grid[nx][ny] === 1) {
+//                     queue.push([nx, ny, minute + 1]);
+//                     grid[nx][ny] = 2;
+//                     fresh--;
+//                 }
+//             }
+
+//             if (queue.length === 0) {
+//                 answer = minute;
+//             };
+//         }
+
+//     if (fresh !== 0) return -1;
+
+//     return answer;
+// };
+
 var orangesRotting = function(grid) {
-    const row = grid.length;
-    const col = grid[0].length;
-    const visited = Array.from({ length: row }, () => Array(col).fill(false));
-    const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
-    let answer = 0;
+  const m = grid.length;
+  const n = grid[0].length;
+  const queue = [];
+  const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+  let fresh = 0;
+  let answer = 0;
 
-    const bfs = (start) => {
-        const queue = [start];
-        const [x, y] = start;
-        visited[x][y] = true;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] === 2) {
+        queue.push([i, j, 0]);
+      } else if (grid[i][j] === 1) {
+        fresh++;
+      }
+    }
+  }
 
-        while (queue.length > 0) {
-            const [x, y, minute] = queue.shift();
+  while (queue.length > 0) {
+    const [x, y, minute] = queue.shift();
+    
+    for (const [dx, dy] of directions) {
+      const nx = x + dx;
+      const ny = y + dy;
 
-            for (const [dx, dy] of directions) {
-                const nx = x + dx;
-                const ny = y + dy;
-
-                if (nx >= 0 && ny >= 0 && nx < row && ny < col && grid[nx][ny] === 1) {
-                    queue.push([nx, ny, minute + 1]);
-                    grid[nx][ny] = 2;
-                    visited[nx][ny] = true;
-                }
-            }
-
-            if (queue.length === 0) return minute;
-        }
+      if (nx >= 0 && ny >= 0 && nx < m && ny < n && grid[nx][ny] === 1) {
+        queue.push([nx, ny, minute + 1]);
+        grid[nx][ny] = 2;
+        fresh--;
+      }
     }
 
-    for (let i = 0; i < row; i++) {
-        for (let j = 0; j < col; j++) {
-            if (grid[i][j] === 2 && !visited[i][j]) {
-                answer += bfs([i, j, 0]);
-            } else {
-                continue;
-            }
-        }
+    if (queue.length === 0) {
+      answer = minute;
     }
+  }
 
-    for (let k = 0; k < row; k++) {
-        for (let l = 0; l < col; l++) {
-            if (grid[k][l] === 1) return -1;
-        }
-    }
+  if (fresh > 0) return -1;
 
-    return answer;
-};
+  return answer;
+}
